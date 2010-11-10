41b8cbac2a4ea5da653d1dc5b7a7a9a60dc8a2e1
Wan Yong <wanyong@gmail.com>
2010-11-10 17:08:36 +0800

    add test100.c to repo

---
+++ test100.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+
+main()
+{
+	int m,k,i,n=0;
+	
+	for(m = 101;m <= 200; m+=2)
+	{
+		k = sqrt(m);
+		for(i = 2;i <= k;i++)
+			if(m % i == 0) break;
+		if(i == k+1)
+		{
+			printf("%d ",m);
+			n++;
+			if(n % 10 == 0)
+				printf("\n");
+		}
+	}
+
+	printf("\n");
+}
